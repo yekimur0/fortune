dbbd3088396b157702856c3cadbe721d0d4282a8
yekimur0 <82269937+yekimur0@users.noreply.github.com>
2024-12-18 00:06:18 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// надписи и цвета на секторах
+
 const prizes = [
   { text: "Скидка 10%", color: "#FFD130", win: true },
   { text: "Дизайн в подарок", color: "#F5F5F5", win: true },
@@ -12,17 +12,14 @@ const prizes = [
   { text: "Скидка 30% на всё", color: "#F5F5F5", win: false }
 ];
 
-// создаём переменные для быстрого доступа ко всем объектам на странице
+
 const wheel = document.querySelector(".deal-wheel");
 const spinner = wheel.querySelector(".spinner");
 const trigger = wheel.querySelector(".btn-spin");
 const ticker = wheel.querySelector(".ticker");
 
-// на сколько секторов нарезаем круг
 const prizeSlice = 360 / prizes.length;
-// на какое расстояние смещаем сектора друг относительно друга
 const prizeOffset = Math.floor(180 / prizes.length);
-// CSS-классы для стилей
 const spinClass = "is-spinning";
 const selectedClass = "selected";
 const spinnerStyles = window.getComputedStyle(spinner);
@@ -32,10 +29,9 @@ let rotation = 0;
 let currentSlice = 0;
 let prizeNodes;
 
-// Фильтруем выигрышные призы
+// Надо получать призы только с win true
 const winPrizes = prizes.filter(prize => prize.win);
 
-// расставляем текст по секторам
 const createPrizeNodes = () => {
   prizes.forEach(({ text, color }, i) => {
     const rotation = ((prizeSlice * i) * -1) - prizeOffset;
@@ -50,7 +46,6 @@ const createPrizeNodes = () => {
   });
 };
 
-// рисуем разноцветные секторы
 const createConicGradient = () => {
   spinner.setAttribute(
     "style",
@@ -64,25 +59,21 @@ const createConicGradient = () => {
   );
 };
 
-// Подготовка колеса
 const setupWheel = () => {
   createConicGradient();
   createPrizeNodes();
   prizeNodes = wheel.querySelectorAll(".prize");
 };
 
-// Выбор случайного выигрышного сектора
 const getWinningPrize = () => {
   const index = Math.floor(Math.random() * winPrizes.length);
   return prizes.indexOf(winPrizes[index]);
 };
 
-// Плавное вращение и остановка на выигрышном секторе
 const spinWheel = () => {
   const selectedPrizeIndex = getWinningPrize();
   let targetRotation = (selectedPrizeIndex * prizeSlice) + prizeOffset;
 
-  // Добавляем случайные обороты для анимации
   const spinInertia = spinertia(5, 10);
   const totalRotation = targetRotation + (360 * spinInertia);
 
@@ -93,7 +84,6 @@ const spinWheel = () => {
   runTickerAnimation();
 };
 
-// Запуск анимации язычка
 const runTickerAnimation = () => {
   const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
   const a = values[0];
@@ -112,7 +102,6 @@ const runTickerAnimation = () => {
   tickerAnim = requestAnimationFrame(runTickerAnimation);
 };
 
-// Завершение вращения
 const endSpin = () => {
   cancelAnimationFrame(tickerAnim);
   rotation %= 360;
@@ -122,7 +111,6 @@ const endSpin = () => {
   spinner.style.setProperty("--rotate", rotation);
 };
 
-// Слушатели событий
 wheel.addEventListener("click", () => {
   prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
   spinWheel();
@@ -130,8 +118,7 @@ wheel.addEventListener("click", () => {
 
 spinner.addEventListener("transitionend", endSpin);
 
-// Запуск
+// init
 setupWheel();
 
-// Генерация случайных оборотов
 const spinertia = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
